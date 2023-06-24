710258459cb8103bfe8e630fc9347d87b2046c1e
SiyoungOh <81408668+siyoungoh@users.noreply.github.com>
2023-06-24 16:55:44 +0900

    Refactoring main.py
    - mma api 조회오류시, 디스코드 메시지로 오류 발생했음 알리기
    - 대소문자 구분없이 선수 이름 검색하기

--- main.py
+++ main.py
@@ -1,134 +1,75 @@
 import discord
 from discord.ext import commands
-from discord import app_commands
 import requests 
 import time
+import json
 
-api_key = 'mma_api_token'
-token = 'discord_bot_token'
+with open('config.json','r') as config_file:
+    config_data = json.load(config_file)
+    
+api_key = config_data['API_KEY']
+token = config_data['DISCORD_TOKEN']
+
+print(api_key, token)
+
 bot = commands.Bot(command_prefix='/',intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print(f'Login bot: {bot.user}')
 
- 
 @bot.command()
-async def hello(message):
-    await message.channel.send('Hi!')
-
-# User input
-search_keyword = 'UFC 273'
-name_keyword = 'Jung'
-  
-# 정보 : MMA API 에서 입력한 시즌 이름의 시즌id 값 가져오기
-
-@bot.command()
-async def mma (message):
-    ######Season Info#########
-    seasons_data = requests.get('http://api.sportradar.us/mma/trial/v2/en/seasons.json?api_key=' + api_key)
-    seasons = seasons_data.json()
-    
-    id_info = ''
-    
-    for i in range(len(seasons['seasons'])):
-        if search_keyword in seasons['seasons'][i]['name']:
-            id_info = seasons['seasons'][i]['id']
-    
-    time.sleep(1)  
-
-    season_info_data = requests.get('http://api.sportradar.us/mma/trial/v2/en/seasons/'+ id_info+'/info.json?api_key='+ api_key)
-    season_info = season_info_data.json()
-    fighter_id = ''
-    for i in range(len(season_info['competitors'])):
-        if name_keyword in season_info['competitors'][i]['name']:
-            fighter_id = season_info['competitors'][i]['id']
+async def mma(message, name_keyword, search_keyword):
+    try:
+        name_keyword = name_keyword.lower()
+        seasons_data = requests.get('http://api.sportradar.us/mma/trial/v2/en/seasons.json?api_key=' + api_key)
+        seasons = seasons_data.json()
         
-    competitor_profile_data = requests.get('http://api.sportradar.us/mma/trial/v2/en/competitors/'+ fighter_id +'/profile.json?api_key='+ api_key)
-    competitor_profile = competitor_profile_data.json() 
-    competitor_name = competitor_profile['competitor']['name']
-    competitor_nickname = competitor_profile['info']['nickname']
-    competitor_gender = competitor_profile['competitor']['gender']
-    competitor_country = competitor_profile['info']['fighting_out_of_country']
-    competitor_reach = competitor_profile['info']['reach']
-    competitor_height = competitor_profile['info']['height']
-    competitor_weight = competitor_profile['info']['weight']
-    competitor_wins = competitor_profile['record']['wins']
-    competitor_draws = competitor_profile['record']['draws']
-    competitor_losses = competitor_profile['record']['losses']
-
-    
-    embed = discord.Embed(title='FIGHTER INFO',
-                          description=f"This is '{competitor_name}''s info")
-    embed.add_field(name='Name', value=f'{competitor_name}', inline=False)
-    embed.add_field(name='Nickname', value=f'{competitor_nickname}', inline=False)
-    embed.add_field(name='Gender', value=f'{competitor_gender}', inline=False)
-    embed.add_field(name='Fighting out of country', value=f'{competitor_country}', inline=False)
-    embed.add_field(name='Reach', value=f'{competitor_reach}', inline=False)
-    embed.add_field(name='Height', value=f'{competitor_height}', inline=False)
-    embed.add_field(name='Weight', value=f'{competitor_weight}', inline=False)
-    embed.add_field(name='Record', value=f'wins: {competitor_wins}, Draws: {competitor_draws}, losses: {competitor_losses}', inline=False)
-    
-    
-
-
-    await message.send(embed=embed)
-
-@bot.command()
-async def mmas(message, name_keyword, search_keyword):
-    seasons_data = requests.get('http://api.sportradar.us/mma/trial/v2/en/seasons.json?api_key=' + api_key)
-    seasons = seasons_data.json()
-   
-       
-    # await message.send(embed=embed)
-    # /echo something some
-    # param1 = 성 (이름)
-    #pram 2 = 그사람의 메인 이벤트 (예: UFC ###)
-
-    
-    id_info = ''
-    
-    for i in range(len(seasons['seasons'])):
-        if search_keyword in seasons['seasons'][i]['name']:
-            id_info = seasons['seasons'][i]['id']
-    
-    time.sleep(1)  
-
-    season_info_data = requests.get('http://api.sportradar.us/mma/trial/v2/en/seasons/'+ id_info+'/info.json?api_key='+ api_key)
-    season_info = season_info_data.json()
-    fighter_id = ''
-    for i in range(len(season_info['competitors'])):
-        if name_keyword in season_info['competitors'][i]['name']:
-            fighter_id = season_info['competitors'][i]['id']
+        id_info = ''
         
-    competitor_profile_data = requests.get('http://api.sportradar.us/mma/trial/v2/en/competitors/'+ fighter_id +'/profile.json?api_key='+ api_key)
-    competitor_profile = competitor_profile_data.json() 
-    competitor_name = competitor_profile['competitor']['name']
-    competitor_nickname = competitor_profile['info']['nickname']
-    competitor_gender = competitor_profile['competitor']['gender']
-    competitor_country = competitor_profile['info']['fighting_out_of_country']
-    competitor_reach = competitor_profile['info']['reach']
-    competitor_height = competitor_profile['info']['height']
-    competitor_weight = competitor_profile['info']['weight']
-    competitor_wins = competitor_profile['record']['wins']
-    competitor_draws = competitor_profile['record']['draws']
-    competitor_losses = competitor_profile['record']['losses']
+        for i in range(len(seasons['seasons'])):
+            if search_keyword in seasons['seasons'][i]['name']:
+                id_info = seasons['seasons'][i]['id']
+        
+        time.sleep(1)  
 
-    
-    embed = discord.Embed(title='FIGHTER INFO',
-                          description=f"This is '{competitor_name}''s info")
-    embed.add_field(name='Name', value=f'{competitor_name}', inline=False)
-    embed.add_field(name='Nickname', value=f'{competitor_nickname}', inline=False)
-    embed.add_field(name='Gender', value=f'{competitor_gender}', inline=False)
-    embed.add_field(name='Fighting out of country', value=f'{competitor_country}', inline=False)
-    embed.add_field(name='Reach', value=f'{competitor_reach}', inline=False)
-    embed.add_field(name='Height', value=f'{competitor_height}', inline=False)
-    embed.add_field(name='Weight', value=f'{competitor_weight}', inline=False)
-    embed.add_field(name='Record', value=f'wins: {competitor_wins}, Draws: {competitor_draws}, losses: {competitor_losses}', inline=False)
-    
-    
+        season_info_data = requests.get('http://api.sportradar.us/mma/trial/v2/en/seasons/'+ id_info+'/info.json?api_key='+ api_key)
+        season_info = season_info_data.json()
+        fighter_id = ''
+        for i in range(len(season_info['competitors'])):
+            if name_keyword in season_info['competitors'][i]['name'].lower():
+                fighter_id = season_info['competitors'][i]['id']
+            
+        competitor_profile_data = requests.get('http://api.sportradar.us/mma/trial/v2/en/competitors/'+ fighter_id +'/profile.json?api_key='+ api_key)
+        competitor_profile = competitor_profile_data.json() 
+        competitor_name = competitor_profile['competitor']['name']
+        competitor_nickname = competitor_profile['info']['nickname']
+        competitor_gender = competitor_profile['competitor']['gender']
+        competitor_country = competitor_profile['info']['fighting_out_of_country']
+        competitor_reach = competitor_profile['info']['reach']
+        competitor_height = competitor_profile['info']['height']
+        competitor_weight = competitor_profile['info']['weight']
+        competitor_wins = competitor_profile['record']['wins']
+        competitor_draws = competitor_profile['record']['draws']
+        competitor_losses = competitor_profile['record']['losses']
 
+        
+        embed = discord.Embed(title='FIGHTER INFO',
+                            description=f"This is '{competitor_name}''s info")
+        embed.add_field(name='Name', value=f'{competitor_name}', inline=False)
+        embed.add_field(name='Nickname', value=f'{competitor_nickname}', inline=False)
+        embed.add_field(name='Gender', value=f'{competitor_gender}', inline=False)
+        embed.add_field(name='Fighting out of country', value=f'{competitor_country}', inline=False)
+        embed.add_field(name='Reach', value=f'{competitor_reach}', inline=False)
+        embed.add_field(name='Height', value=f'{competitor_height}', inline=False)
+        embed.add_field(name='Weight', value=f'{competitor_weight}', inline=False)
+        embed.add_field(name='Record', value=f'wins: {competitor_wins}, Draws: {competitor_draws}, losses: {competitor_losses}', inline=False)
 
-    await message.send(embed=embed)
+        await message.send(embed=embed)
+    
+    except Exception as e:
+        print(e)
+        await message.channel.send("Please Check For Any Spelling Errors")
+
 bot.run(token)
- 
+
